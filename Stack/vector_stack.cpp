931862d36092155f4f4c21e5051e245f3d3a63f2
HarshVardhan Sahu <harshvardhansahoo7115@gmail.com>
2025-09-04 18:30:19 +0530

    stack

---
+++ Stack/vector_stack.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Stack{
+ vector<int> v;
+ public:
+  void push(int  val){//O(1)
+   v.push_back(val);
+  }
+  void pop(){//O(1)
+    v.pop_back();
+  }
+  int top(){//O(1)
+    return v[v.size()-1];
+  }
+  bool empty(){
+    return v.size()==0;
+  }
+};
+
+int main(){
+Stack s;
+s.push(1);
+s.push(10);
+s.push(100);
+while(!s.empty()){
+    cout<<s.top()<<endl;
+    s.pop();
+}
+cout<<endl;
+}
