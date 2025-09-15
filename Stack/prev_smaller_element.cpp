5488e0995ff8c3221dd2f0c88556e533e7598a59
HarshVardhan Sahu <harshvardhansahoo7115@gmail.com>
2025-09-16 00:44:38 +0530

    clear

--- Stack/prev_smaller_element.cpp
+++ Stack/prev_smaller_element.cpp
@@ -4,7 +4,20 @@
 using namespace std;
 
 vector<int> prevsmaller(vector<int> arr){
-
+  vector<int> ans(arr.size(),0);
+  stack<int> s;
+  for(int i=0;i<arr.size();i++){
+    while(s.size()>0&&s.top()>=arr[i]){
+        s.pop();
+    }
+    if(s.empty()){
+        ans[i]=-1;
+    }else{
+        ans[i]=s.top();
+    }
+    s.push(arr[i]);
+  }
+  return ans;
 }
 
 int main(){
@@ -12,7 +25,7 @@ int main(){
     vector<int> ans=prevsmaller(arr);
 
     for(int val:ans){
-        cout<<val<" ";
+        cout<<val<<" ";
     }
     cout<<endl;
     return 0;
